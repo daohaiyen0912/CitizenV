0c1d5150699f720bf5b7d16774ba0da73e207b73
daohaiyen0912 <71396759+daohaiyen0912@users.noreply.github.com>
2021-12-02 22:01:21 +0700

    2/12/2021

--- view/js/quanli.js
+++ view/js/quanli.js
@@ -3,4 +3,15 @@ const bar = (
         <img src="img/CitizenVbar2.png"></img>
     </div>
 )
+
+const sidebar = (
+    <div id="sidebar">
+        <div>
+            <img></img>
+            <h2>Cán bộ A1</h2>
+            <h3>Tổng cục dân số</h3>
+        </div>
+    </div>
+)
+
 ReactDOM.render(bar, document.getElementById('root'));
